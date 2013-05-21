9e81129c29d9362798f44e1869a1ddbc977d9d80
Kazuho Yamaguchi <kyamaguchi@mediweb.jp>
2013-05-21 20:42:10 +0900

    Add scroll view

--- app/jenkins_controller.rb
+++ app/jenkins_controller.rb
@@ -2,44 +2,53 @@ class JenkinsController < NSWindowController
   attr_reader :window
 
   def init
+    @feed = []
     super
     buildWindow
-    @feed = []
-
-    @table_view = NSTableView.alloc.init
-    column_name = NSTableColumn.alloc.initWithIdentifier("name")
-    column_name.editable = false
-    column_name.headerCell.title = "Name"
-    column_name.width = 150
-    @table_view.addTableColumn(column_name)
-
-    column_color = NSTableColumn.alloc.initWithIdentifier("color")
-    column_color.editable = false
-    column_color.headerCell.title = "Status"
-    column_color.width = 150
-    @table_view.addTableColumn(column_color)
-
-    @table_view.delegate = self
-    @table_view.dataSource = self
-    @window.contentView.addSubview(@table_view)
-
     fetchStatus
     self
   end
 
   def buildWindow
-    @window = NSWindow.alloc.initWithContentRect([[240, 180], [480, 360]],
+    scroll_view_height = 320
+    @window = NSWindow.alloc.initWithContentRect([[240, 180], [432, scroll_view_height + 40]],
       styleMask: NSTitledWindowMask|NSClosableWindowMask|NSMiniaturizableWindowMask|NSResizableWindowMask,
       backing: NSBackingStoreBuffered,
       defer: false)
     @window.title = NSBundle.mainBundle.infoDictionary['CFBundleName']
     @window.orderFrontRegardless
+
+    scroll_view = NSScrollView.alloc.initWithFrame(NSMakeRect(20, 20, 392, scroll_view_height))
+    scroll_view.setAutoresizingMask(NSViewWidthSizable|NSViewHeightSizable)
+    scroll_view.setHasVerticalScroller(true)
+    scroll_view.setHasHorizontalScroller(true)
+    scroll_view.setBorderType(NSBezelBorder)
+    @window.contentView.addSubview(scroll_view)
+
+    @myTableView = NSTableView.alloc.init
+    @myTableView.setUsesAlternatingRowBackgroundColors(true)
+    scroll_view.setDocumentView(@myTableView)
+
+    column_name = NSTableColumn.alloc.initWithIdentifier("name")
+    column_name.editable = false
+    column_name.headerCell.title = "Name"
+    column_name.width = 150
+    @myTableView.addTableColumn(column_name)
+
+    column_color = NSTableColumn.alloc.initWithIdentifier("color")
+    column_color.editable = false
+    column_color.headerCell.title = "Status"
+    column_color.width = 150
+    @myTableView.addTableColumn(column_color)
+
+    @myTableView.delegate = self
+    @myTableView.dataSource = self
   end
 
   def fetchStatus
     BW::HTTP.get('http://jenkins.local:8088/api/json') do |r|
       @feed = BW::JSON.parse(r.body)['jobs']
-      @table_view.reloadData
+      @myTableView.reloadData
     end
   end
 
