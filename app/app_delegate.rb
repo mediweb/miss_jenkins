00587476034fa33730f2d7c3ef53713085ea1ed4
Yuri Kovalov <yuri@yurikoval.com>
2013-08-14 16:44:14 +0900

    Trigger auto refresh.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,5 +2,11 @@ class AppDelegate
   def applicationDidFinishLaunching(notification)
     buildMenu
     @jenkinsController = JenkinsController.alloc.init
+    trigger_refresh
+  end
+
+  def trigger_refresh
+    timer = NSTimer.timerWithTimeInterval(5*60, target:@jenkinsController, selector:'refresh_status:', userInfo:nil, repeats:true)
+    NSRunLoop.mainRunLoop.addTimer(timer, forMode:NSRunLoopCommonModes)
   end
 end
